fb94fb24f71651d817996dc2b5e84d4dfd23dd76
Robson Costa <robson.costa@ifsc.edu.br>
2023-09-19 14:17:32 -0300

    build(speaker): Created speaker function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include "led.h"
 
 unsigned long led_last_time = 0;
+unsigned long speaker_last_time = 0;
+double speaker_freq = 78;
+unsigned long speaker_duration = 300;
+unsigned long speaker_period = 900;
 
 void setup() {
   Serial.begin(SERIAL_BAUDRATE);
@@ -14,8 +18,17 @@ void setup() {
 }
 
 void loop() {
+
+  /* Led looping */
   if (millis() - led_last_time >= LED_INTERVAL) {
     led_last_time = millis();
     led_blink();
+  }
+
+  /* Speaker looping */
+  if (millis() - speaker_last_time > speaker_period) {
+    speaker_last_time = millis();
+    tone(SPEAKER_PIN, speaker_freq, speaker_duration);
+    noTone(SPEAKER_PIN);
   }
 }
